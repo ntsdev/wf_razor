56d8813591c41761c8e5fa43c13c031ee926dcd3
govardhannts <panaazcs@gmail.com>
2018-09-09 18:38:45 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace wfproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
